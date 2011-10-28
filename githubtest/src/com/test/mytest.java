cff5b2bf59fb27071b5d0ccf98ec239a70aae1a0
Naveen.A.N <naveen.ann@gmail.com>
2011-10-28 12:05:32 +0530

    Init commit

---
+++ githubtest/src/com/test/mytest.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class mytest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my test to push to github");
+
+	}
+
+}
